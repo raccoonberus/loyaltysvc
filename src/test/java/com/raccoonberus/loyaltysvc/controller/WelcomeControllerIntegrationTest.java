71d5e38ef31aeb54069051859174d4b16afb0f46
GoForBroke1006 <go.for.broke1006@gmail.com>
2018-06-29 04:44:47 +0300

    Initial commit

--- src/test/java/com/raccoonberus/loyaltysvc/controller/WelcomeControllerIntegrationTest.java
+++ src/test/java/com/raccoonberus/loyaltysvc/controller/WelcomeControllerIntegrationTest.java
@@ -1,27 +1,13 @@
 package com.raccoonberus.loyaltysvc.controller;
 
-import com.raccoonberus.loyaltysvc.Application;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mock.web.MockServletContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import javax.servlet.ServletContext;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 public class WelcomeControllerIntegrationTest extends BaseIntegrationTest {
