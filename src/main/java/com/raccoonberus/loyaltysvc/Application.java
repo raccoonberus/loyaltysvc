eaa45122ed015397da705a98c6ae4f4c3faa1b48
Sergey <go.for.broke1006@gmail.com>
2018-06-29 16:57:27 +0300

    Update Application.java

--- src/main/java/com/raccoonberus/loyaltysvc/Application.java
+++ src/main/java/com/raccoonberus/loyaltysvc/Application.java
@@ -1,11 +1,20 @@
 package com.raccoonberus.loyaltysvc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class 
+})
 public class Application {
 
     public static void main(String[] args) {
